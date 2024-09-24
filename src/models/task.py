ba9d049ecdd40e26b47e8ea70ef4642834ea7112
Cory Eighan <zephyr834@gmail.com>
2024-09-24 02:05:58 -0700

    Refactor pritn and datetime readable function to taskHelper

--- src/models/task.py
+++ src/models/task.py
@@ -13,17 +13,3 @@ def task(title, description):
         "createdAt": datetime.now().isoformat(),
         "updatedAt": datetime.now().isoformat()
     }
-
-def getReadableDateTime(isoDateTime):
-    # Convert the ISO string to a datetime object
-    dt_object = datetime.fromisoformat(isoDateTime)
-
-    readable_format = dt_object.strftime("%B %d, %Y %I:%M %p")
-    return readable_format
-
-def printTasks(tasks):
-    print(f"{'UID':^15} | {'Title':^15}| {'Description':^15}| {'Status':^15}| {'Created At':^30}| {'Updated At':^30}|")
-    for t in tasks:
-        createdAt = getReadableDateTime(t['createdAt'])
-        updatedAt = getReadableDateTime(t['createdAt'])
-        print(f"{t['uid']:^15} | {t['title']:^15}| {t['description']:^15}| {t['status']:^15}| {createdAt:^30}| {updatedAt:^30}|")
